a68da1b798b52c9800233bddda402e94a772cc4d
Givi Gelashvili <givi.gelashvili@adjarabet.com>
2021-10-27 17:32:26 +0400

    Styling

--- src/App/App.purs
+++ src/App/App.purs
@@ -2,6 +2,7 @@ module App (component) where
 
 import Prelude
 import App.Components.Dropdown as Dropdown
+import App.Internal.CSS (css)
 import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
@@ -36,11 +37,16 @@ initialState _ = { initialized: false }
 render :: forall m. MonadAff m => State -> H.ComponentHTML Action Slots m
 render _ =
   HH.div_
-    [ HH.slot Dropdown._dropdown unit Dropdown.dropdown
-        { title: "MicroFe"
-        , options: [ { title: "VirtualSport", value: 1 } ]
-        }
-        HandleDropdown
+    [ HH.div [ css "_s_bd-primary-5 _s_bd-solid _s_bw-1 _s_container _s_flex _s_p-6 _s_size-w-percent--25" ]
+        [ HH.span [ css "_s_label _s_label-lg _s_label-font-setting-case-on" ] [ HH.text "Adjarabet Micro-Frontend Central" ] ]
+    , HH.div [ css "_s_bd-primary-5 _s_bd-solid _s_bw-1 _s_container _s_flex _s_mb-6 _s_p-6 _s_size-w-percent--25" ]
+        [ HH.span [ css "_s_label _s_label-md _s_label-font-setting-case-on" ]
+            [ HH.text "Choose Project To Deploy"
+            , HH.slot Dropdown._dropdown unit Dropdown.dropdown
+                [ { title: "VirtualSport", value: 1 } ]
+                HandleDropdown
+            ]
+        ]
     ]
 
 handleAction :: forall o m. MonadAff m => Action -> H.HalogenM State Action Slots o m Unit
--- src/App/Components/Dropdown.purs
+++ src/App/Components/Dropdown.purs
@@ -31,13 +31,10 @@ type State
   = { opened :: Boolean
     , active :: Maybe Option
     , options :: Options
-    , title :: String
     }
 
 type Input
-  = { title :: String
-    , options :: Options
-    }
+  = Options
 
 data Output
   = Changed Int
@@ -56,45 +53,37 @@ dropdown =
     }
   where
   initialState :: Input -> State
-  initialState input =
-    { active: head input.options
-    , options: input.options
+  initialState options =
+    { active: head options
+    , options: options
     , opened: false
-    , title: input.title
     }
 
   render :: State -> H.ComponentHTML Action () m
   render state =
-    HH.div [ css "_s_col-6" ]
-      [ HH.div [ css "_s_flex _s_flex-a-center" ]
-          [ HH.div [ css "_s_color-primary-8 _s_label _s_label-sm _s_mr-2" ]
-              [ HH.span_ [ HH.text state.title ]
-              ]
-          , HH.div
-              [ css "_s_flex-a-start-i _s_input _s_input-sm _s_mr-3 _s_overflow-visible _s_p-none _s_position-relative _s_size-w-min-px--53 _s_valid"
-              , HE.onClick \_ -> Toggle
-              ]
-              [ HH.h3 [ css "_s_label _s_label-sm _s_size-h-percent--25 _s_m-none _s_size-w-percent--25 _s_pt-2 _s_pl-2 _s_label-400 _s_size-w-min-percent--25 _s_aitem-pt-none" ]
-                  [ HH.span_ [ whenElem' state.active (\s -> s.title) ]
-                  ]
-              , HH.h5
-                  [ css "_s_position-absolute   _s_position-l-percent--0 _s_position-t-percent--25 _s_m-none _s_z-2 _s_color-bg-primary-4 _s_size-w-percent--25 _s_b-radisu-sm _s_oveflow-hidden"
-                  , toggleVisibility state.opened
-                  ]
-                  [ HH.h4 [ css "_s_size-h-max-px--70 _s_m-none _s_overflow-x-auto _s_overflow-hidden _s_pt-2" ]
-                      $ ( \e ->
-                            HH.span
-                              [ css "_s_pl-2 _s_pr-2 _s_mb-2 _s_label _s_label-sm _s_color-primary-8 _s_cursor-pointer _s_h-color _s_hitem-color-primary-1 _s_transition-0--2 _s_label-400 active"
-                              , HE.onClick \_ -> Choose e
-                              ]
-                              [ HH.text e.title ]
-                        )
-                      <$> state.options
-                  , HH.h6 [ css "_s_size-w-percent--25 _s_m-none _s_position-relative" ]
-                      [ HH.span [ css "_s_position-absolute _s_position-r-px--0 _s_position-t-px--0 _s_pt-3 _s_pointer-event-none" ]
-                          [ HH.span [ css "_s_icon _s_icon-xs _s_adj-arrow-down _s_color-primary-8" ] []
-                          ]
+    HH.div
+      [ css "_s_flex-a-start-i _s_input _s_input-sm _s_ml-3 _s_overflow-visible _s_p-none _s_position-relative _s_size-w-min-px--53 _s_valid"
+      , HE.onClick \_ -> Toggle
+      ]
+      [ HH.h3 [ css "_s_label _s_label-sm _s_size-h-percent--25 _s_m-none _s_size-w-percent--25 _s_pt-2 _s_pl-2 _s_label-400 _s_size-w-min-percent--25 _s_aitem-pt-none" ]
+          [ HH.span_ [ whenElem' state.active (\s -> s.title) ]
+          ]
+      , HH.h5
+          [ css "_s_position-absolute   _s_position-l-percent--0 _s_position-t-percent--25 _s_m-none _s_z-2 _s_color-bg-primary-4 _s_size-w-percent--25 _s_b-radisu-sm _s_oveflow-hidden"
+          , toggleVisibility state.opened
+          ]
+          [ HH.h4 [ css "_s_size-h-max-px--70 _s_m-none _s_overflow-x-auto _s_overflow-hidden _s_pt-2" ]
+              $ ( \e ->
+                    HH.span
+                      [ css "_s_pl-2 _s_pr-2 _s_mb-2 _s_label _s_label-sm _s_color-primary-8 _s_cursor-pointer _s_h-color _s_hitem-color-primary-1 _s_transition-0--2 _s_label-400 active"
+                      , HE.onClick \_ -> Choose e
                       ]
+                      [ HH.text e.title ]
+                )
+              <$> state.options
+          , HH.h6 [ css "_s_size-w-percent--25 _s_m-none _s_position-relative" ]
+              [ HH.span [ css "_s_position-absolute _s_position-r-px--0 _s_position-t-px--0 _s_pt-3 _s_pointer-event-none" ]
+                  [ HH.span [ css "_s_icon _s_icon-xs _s_adj-arrow-down _s_color-primary-8" ] []
                   ]
               ]
           ]
